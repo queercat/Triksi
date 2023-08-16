aa2061e396d994ae9d17daaa7a37623cf0ef9c95
Artyom Kuznetsov <hi@wzrd.ht>
2023-08-16 12:24:18 -0400

    Use capture groups to extract the query

--- bot/src/handlers/messageHandler.ts
+++ bot/src/handlers/messageHandler.ts
@@ -1,19 +1,17 @@
 import { Message } from "discord.js"
 
 // Regex to match [[text]] but not [[text[[text]]text]]]] only [[text]] or [[ text ]] or [[ text 123 ]]
-const regex = /\[\[([^\[\]]+)\]\]/g
+const regex = /\[\[(?<query>[^\[\]]+)\]\]/g
 
 export const messageHandler = async (message: Message) => {
   const content = message.content
   let cardNames = []
 
   if (content.match(regex)) {
-    const matches = content.match(regex)
+    const matches = content.matchAll(regex)
     if (matches) {
       for (const match of matches) {
-        const text = match.replace("[[", "").replace("]]", "")
-
-        cardNames.push(text)
+        cardNames.push(match.groups.query)
       }
     }
   }
