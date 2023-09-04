e86dfb0dbbf7e511734fb43cb76930dbd57671c9
May Tusek <tusek.may@gmail.com>
2023-09-03 19:55:53 -0700

    Cleans up handling.

--- bot/src/handlers/messageHandler.ts
+++ bot/src/handlers/messageHandler.ts
@@ -1,36 +1,8 @@
 import { Message } from "discord.js"
-
-// Regex to match [[text]] but not [[text[[text]]text]]]] only [[text]] or [[ text ]] or [[ text 123 ]]
-const regex = /\[\[([^\[\]]+)\]\]/g
+import { cardFetcher } from "./executors/cardFetcher"
 
 export const messageHandler = async (message: Message) => {
-  const content = message.content
-  let cardNames = []
+  const content = message.content;
 
-  if (content.match(regex)) {
-    const matches = content.match(regex)
-    if (matches) {
-      for (const match of matches) {
-        const text = match.replace("[[", "").replace("]]", "")
-
-        cardNames.push(text)
-      }
-    }
-  }
-
-  if (cardNames.length === 0) return
-
-  let reply = ""
-
-  cardNames.forEach((cardName) => {
-    const card = cardNameMap.get(cardName) 
-
-    if (card) {
-      reply += card.faces[0].image + "\n"
-    }
-  })
-
-  if (reply.length > 0) {
-    message.reply(reply)
-  }
+  cardFetcher(message, content)
 }
